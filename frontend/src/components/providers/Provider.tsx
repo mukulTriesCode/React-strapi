41be1926cd7ae5e0714db63ca7bfe2a34356f0a3
Mukul Sharma <mukulsharma262002@gmail.com>
2025-12-08 12:57:39 +0530

    Add GraphQLProvider to main application entry point and remove ApolloProvider from Provider component for streamlined data handling.

--- frontend/src/components/providers/Provider.tsx
+++ frontend/src/components/providers/Provider.tsx
@@ -1,9 +1,7 @@
-import { ApolloProvider } from "@apollo/client/react";
 import React from "react";
-import { client } from "../../utils/apolloClient";
 
 const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return <>{children}</>;
 };
 
 export default Provider;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { GraphQLProvider } from './graphql/client';
+
+const endpoint = import.meta.env.VITE_BACKEND_BASE_URL?.replace(/\/+$/, "") + "/graphql" || "http://localhost:1337/graphql";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GraphQLProvider endpoint={endpoint}>
+      <App />
+    </GraphQLProvider>
   </StrictMode>,
-)
+);
