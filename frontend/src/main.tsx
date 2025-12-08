02d76c4e0c41308cdc4c0ba32f863b0f6fa6f881
Mukul Sharma <mukulsharma262002@gmail.com>
2025-12-08 13:07:44 +0530

    Refactor main.tsx to remove GraphQLProvider and streamline the application entry point.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { GraphQLProvider } from './graphql/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-const endpoint = import.meta.env.VITE_BACKEND_BASE_URL?.replace(/\/+$/, "") + "/graphql" || "http://localhost:1337/graphql";
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GraphQLProvider endpoint={endpoint}>
-      <App />
-    </GraphQLProvider>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 );
